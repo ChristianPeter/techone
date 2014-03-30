0c44e9d9b6efab8e5c9edfd95f2f72f2170c851f
Christian Peter <peter.chr@web.de>
2014-03-30 11:41:41 +0200

    Revert "Revert "cleanup and add clean-force param to the grunt config""
    This reverts commit ca8d10d3330d5aab5a09048d683a314f8360509d.

--- src/main/webapp_grunt/Gruntfile.js
+++ src/main/webapp_grunt/Gruntfile.js
@@ -115,6 +115,7 @@ module.exports = function (grunt) {
 
     // Empties folders to start fresh
     clean: {
+      options: { force: true },
       dist: {
         files: [{
           dot: true,
